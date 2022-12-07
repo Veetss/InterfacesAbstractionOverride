1d5d7e70e7d0485f3cfc4fa07d3d664514dbfa14
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:36:03 +0100

    Update Vehicle.java

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Vehicle {
+public abstract class Vehicle{
+
+    public String type;
+    public int numberOfWheels;
+
+
+    public void showVehicleDetails() {
+        System.out.println("This veichle is a " + type + " and have " + numberOfWheels + " number of wheels");
+    }
+
+    public abstract void doVehicleSound();
 }
