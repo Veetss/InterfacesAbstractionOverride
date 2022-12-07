4ee52357a564c212addc4b63235b097cc9515add
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:35:35 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
+
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car panda = new Car(4, 3, 18.050);
+        Boat barca = new Boat(30.1,  1000);
+
+        panda.showVehicleDetails();
+
+        barca.getBoatWeightAndSpeed();
     }
 }
