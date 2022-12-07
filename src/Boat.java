1007148111c21dc137dcc4bc1c1a581a87911cb6
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:34:37 +0100

    Update Boat.java

--- src/Boat.java
+++ src/Boat.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Boat {
+public class Boat extends Vehicle{
+
+    public double maxKnotsSpeed;
+    public int boatKilosWeight;
+
+    public void doVehicleSound() {
+        System.out.println("Java boat go swush");
+    }
+
+    /**
+     *
+     * @param maxSpeed
+     * @param weight
+     */
+    public Boat(double maxSpeed, int weight) {
+        super.type = "Boat";
+        this.maxKnotsSpeed = maxSpeed;
+        this.boatKilosWeight = weight;
+    }
+
+    public void getBoatWeightAndSpeed() {
+        super.showVehicleDetails();
+        System.out.println("Knot speed is: " + maxKnotsSpeed + " and the weight is: " + boatKilosWeight + "kg");
+    }
 }
