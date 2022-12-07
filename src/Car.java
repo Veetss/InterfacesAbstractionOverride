a6030cd4761be1ef559b2110dc6cc2affd0312c0
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:35:10 +0100

    Update Car.java

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Car {
+public class Car extends Vehicle {
+
+    public int numberOfDoors;
+    public double carPrice;
+
+    public void showVehicleDetails() {
+        super.showVehicleDetails();
+        System.out.println("Number of doors is: " + this.numberOfDoors);
+    }
+
+    public void doVehicleSound() {
+        System.out.println("Java car go brr");
+    }
+
+    /**
+     *
+     * @param wheels
+     * @param doors
+     * @param price
+     */
+    public Car(int wheels, int doors, double price) {
+        super.type = "Car";
+        this.numberOfWheels = wheels;
+        this.numberOfDoors = doors;
+        this.carPrice = price;
+    }
 }
